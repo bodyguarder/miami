2bca4e7dea8deb762542426224057b7038fd7654
Erlan <erlannasyrov9@gmail.com>
2023-07-06 15:48:16 +0600

    finish header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
-/* $('.').on('click', function () {
-  
-}); */
+$(function () {
+  $('.menu-button').on('click', function () {
+    $('.menu-button').toggleClass('menu-button--active');
+    $('.header__wrapper').toggleClass('header__wrapper--active');
+    $('body').toggleClass('hidden');
+  })
+});
